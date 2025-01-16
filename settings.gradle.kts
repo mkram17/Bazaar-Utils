c1eda12454c9bccc7fdd7f2a1b9a396cc8d1f1c8
SirMega-Bite <1maxkramer@gmail.com>
2025-01-15 20:00:23 -0500

    Uses DGT (MIXINS DONT WORK)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,19 +8,31 @@ pluginManagement {
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/maven/")
         maven("https://repo.sk1er.club/repository/maven-releases/")
+
+
+        //testing
+        // Releases
+        maven("https://maven.deftu.dev/releases")
+        maven("https://repo.essential.gg/repository/maven-public")
+        maven("https://server.bbkr.space/artifactory/libs-release/")
+        maven("https://jitpack.io/")
+
+        // Snapshots
+        maven("https://maven.deftu.dev/snapshots")
+        mavenLocal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
-            }
-        }
-    }
+//    resolutionStrategy {
+//        eachPlugin {
+//            when (requested.id.id) {
+//                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
+//            }
+//        }
+//    }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
-}
+//plugins {
+//    id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
+//}
 
 
 rootProject.name = "Bazaar-Utils"
