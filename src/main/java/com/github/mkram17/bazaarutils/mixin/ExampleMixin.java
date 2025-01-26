3d1d44608ea8d151e7d52db327acde4e6cfd006e
SirMega-Bite <1maxkramer@gmail.com>
2025-01-25 21:26:10 -0500

    mostly just gradle setup

--- src/main/java/com/github/mkram17/bazaarutils/mixin/ExampleMixin.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/ExampleMixin.java
@@ -1,15 +1,13 @@
 package com.github.mkram17.bazaarutils.mixin;
 
+import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(MinecraftClient.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+
 }
