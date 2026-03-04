6dc1c0424b43566b817ee3a57996aadfd38c1b0b
mkram17 <1maxkramer@gmail.com>
2026-03-03 21:01:36 -0500

    Separate the market prices of PriceInfo into new MarketPrices class

---
+++ src/main/java/com/github/mkram17/bazaarutils/utils/bazaar/market/price/MarketPrices.java
@@ -0,0 +1,50 @@
+package com.github.mkram17.bazaarutils.utils.bazaar.market.price;
+
+import com.github.mkram17.bazaarutils.utils.Util;
+import com.github.mkram17.bazaarutils.utils.bazaar.data.BazaarDataManager;
+import com.github.mkram17.bazaarutils.utils.bazaar.market.order.OrderType;
+
+public class MarketPrices {
+
+    private final PriceInfo buyPriceInfo = new PriceInfo(null, OrderType.BUY);
+    private final PriceInfo sellPriceInfo = new PriceInfo(null, OrderType.SELL);
+    private final String productID;
+
+    public MarketPrices(String productID) {
+        this.productID = productID;
+        updateMarketPrices(productID);
+    }
+
+    /**
+     * Refreshes cached market price data for this product.
+     */
+    public void updateMarketPrices() {
+        updateMarketPrices(productID);
+    }
+
+    private void updateMarketPrices(String productId) {
+        var buyPriceOpt = BazaarDataManager.findItemPriceOptional(productId, OrderType.BUY);
+        var sellPriceOpt = BazaarDataManager.findItemPriceOptional(productId, OrderType.SELL);
+
+        buyPriceOpt.ifPresent(price -> buyPriceInfo.setPricePerItem(Util.truncateNum(price)));
+        sellPriceOpt.ifPresent(price -> sellPriceInfo.setPricePerItem(Util.truncateNum(price)));
+    }
+
+    public Double getPriceForPosition(PricingPosition pricingPosition, OrderType orderType) {
+        double marketSellPrice = sellPriceInfo.getPricePerItem();
+        double marketBuyPrice = buyPriceInfo.getPricePerItem();
+
+        return switch (orderType) {
+            case SELL -> switch (pricingPosition) {
+                case COMPETITIVE -> marketSellPrice - 0.1;
+                case MATCHED -> marketSellPrice;
+                case OUTBID -> marketSellPrice + 0.1;
+            };
+            case BUY -> switch (pricingPosition) {
+                case COMPETITIVE -> marketBuyPrice + 0.1;
+                case MATCHED -> marketBuyPrice;
+                case OUTBID -> marketBuyPrice - 0.1;
+            };
+        };
+    }
+}
