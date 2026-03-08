67ec7cce2dfc6d367c94a1d103f77e56ccdbf976
mkram17 <1maxkramer@gmail.com>
2026-03-08 15:08:10 -0400

    refactor Bookmark to use MarketPrices

--- src/main/java/com/github/mkram17/bazaarutils/features/gui/buttons/bookmarks/Bookmark.java
+++ src/main/java/com/github/mkram17/bazaarutils/features/gui/buttons/bookmarks/Bookmark.java
@@ -1,5 +1,6 @@
 package com.github.mkram17.bazaarutils.features.gui.buttons.bookmarks;
 
+import com.github.mkram17.bazaarutils.utils.bazaar.market.price.MarketPrices;
 import net.minecraft.item.ItemStack;
 
-public record Bookmark(String name, ItemStack itemStack, String productID) {}
+public record Bookmark(String name, ItemStack itemStack, MarketPrices marketPrices) {}
--- src/main/java/com/github/mkram17/bazaarutils/features/gui/buttons/bookmarks/ToggleBookmarkButton.java
+++ src/main/java/com/github/mkram17/bazaarutils/features/gui/buttons/bookmarks/ToggleBookmarkButton.java
@@ -8,6 +8,7 @@ import com.github.mkram17.bazaarutils.utils.annotations.modules.Module;
 import com.github.mkram17.bazaarutils.utils.bazaar.gui.BazaarScreenHandler;
 import com.github.mkram17.bazaarutils.utils.bazaar.gui.BazaarScreens;
 import com.github.mkram17.bazaarutils.utils.minecraft.item.ItemButton;
+import com.github.mkram17.bazaarutils.utils.bazaar.market.price.MarketPrices;
 import com.github.mkram17.bazaarutils.utils.minecraft.ItemInfo;
 import com.github.mkram17.bazaarutils.utils.minecraft.components.CustomDataComponents;
 import com.github.mkram17.bazaarutils.utils.minecraft.gui.ScreenManager;
@@ -97,7 +98,8 @@ public class ToggleBookmarkButton extends BUListener implements ItemButton {
                     .flatMap(BazaarScreenHandler::getDisplayProductId)
                     .orElse(null);
 
-            Bookmark newBookmark = new Bookmark(name, itemStack, productId);
+            MarketPrices bookmarkMarketPrices = new MarketPrices(productId);
+            Bookmark newBookmark = new Bookmark(name, itemStack, bookmarkMarketPrices);
             list.add(newBookmark);
 
             BookmarkUtil.currentBookmarkOpt = Optional.of(newBookmark);
