8b3e20a82a7220cdeee56f648be805ca5d3114d0
0xar-ds <sadden-rope-sleek@duck.com>
2026-03-22 00:18:59 -0300

    Abstract SlotProviders builder api to a ItemBuilder class
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/config/util/api/SlotProviders.java
+++ src/main/java/com/github/mkram17/bazaarutils/config/util/api/SlotProviders.java
@@ -1,13 +1,9 @@
 package com.github.mkram17.bazaarutils.config.util.api;
 
+import com.github.mkram17.bazaarutils.utils.minecraft.item.ItemBuilder;
 import com.github.mkram17.bazaarutils.utils.minecraft.item.ItemButton;
-import com.github.mkram17.bazaarutils.utils.minecraft.components.CustomDataComponents;
-import it.unimi.dsi.fastutil.objects.ReferenceSortedSets;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 
 import java.util.HashMap;
 import java.util.List;
@@ -54,53 +50,11 @@ public final class SlotProviders {
         return REGISTRY.getOrDefault(key, (slotIndex, selectedSlotIndex) -> ItemStack.EMPTY);
     }
 
-    public static SlotStack stack(Item item) {
-        return new SlotStack(item, 1);
+    public static ItemBuilder stack(Item item) {
+        return ItemBuilder.of(item);
     }
 
-    public static SlotStack stack(Item item, int count) {
-        return new SlotStack(item, count);
-    }
-
-    public static final class SlotStack {
-
-        private final ItemStack stack;
-
-        private SlotStack(Item item, int count) {
-            this.stack = new ItemStack(item, count);
-        }
-
-        public SlotStack named(Text name) {
-            stack.set(DataComponentTypes.CUSTOM_NAME, name);
-            return this;
-        }
-
-        public SlotStack named(String name) {
-            return named(Text.literal(name));
-        }
-
-        public SlotStack locked() {
-            stack.set(CustomDataComponents.SLOT_SELECTOR_LOCKED, true);
-            return this;
-        }
-
-        public SlotStack hideTooltip() {
-            stack.set(DataComponentTypes.TOOLTIP_DISPLAY, new TooltipDisplayComponent(true, ReferenceSortedSets.emptySet()));
-            return this;
-        }
-
-//      we hide attributes by default
-//        public SlotStack hideAttributes() {
-//            TooltipDisplayComponent current = stack.getOrDefault(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplayComponent.DEFAULT);
-//            stack.set(DataComponentTypes.TOOLTIP_DISPLAY, current.with(DataComponentTypes.ATTRIBUTE_MODIFIERS, true));
-//            return this;
-//        }
-
-        public ItemStack build() {
-            TooltipDisplayComponent current = stack.getOrDefault(DataComponentTypes.TOOLTIP_DISPLAY, TooltipDisplayComponent.DEFAULT);
-            stack.set(DataComponentTypes.TOOLTIP_DISPLAY, current.with(DataComponentTypes.ATTRIBUTE_MODIFIERS, true));
-
-            return stack;
-        }
+    public static ItemBuilder stack(Item item, int count) {
+        return ItemBuilder.of(item, count);
     }
 }
