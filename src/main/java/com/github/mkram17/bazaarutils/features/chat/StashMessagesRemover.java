e302677d791d56357cdbad996910940ae995bf1e
0xar-ds <sadden-rope-sleek@duck.com>
2026-04-22 17:58:43 -0300

    Proper logging on chat features
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/features/chat/StashMessagesRemover.java
+++ src/main/java/com/github/mkram17/bazaarutils/features/chat/StashMessagesRemover.java
@@ -2,98 +2,52 @@ package com.github.mkram17.bazaarutils.features.chat;
 
 import com.github.mkram17.bazaarutils.config.features.chat.ChatConfig;
 import com.github.mkram17.bazaarutils.events.BUListener;
-import com.github.mkram17.bazaarutils.utils.PlayerActionUtil;
+import com.github.mkram17.bazaarutils.utils.PlayerLogger;
 import com.github.mkram17.bazaarutils.utils.Util;
 import com.github.mkram17.bazaarutils.utils.annotations.modules.Module;
 import com.github.mkram17.bazaarutils.utils.config.ToggleableFeature;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-
-import java.util.ArrayList;
-import java.util.Map;
+import com.github.mkram17.bazaarutils.utils.minecraft.SequenceChatFilter;
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription;
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock;
+import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent;
 
 @Module
 public class StashMessagesRemover extends BUListener implements ToggleableFeature {
-    private static final Map<String, Integer> REMOVE_MAP = Map.of(
-            "materials stashed away", 0,
-            "types of materials stashed", 1,
-            "to pick them up", 2
-    );
 
-    private static final int SEQUENCE_LENGTH = REMOVE_MAP.size();
+    private static final SequenceChatFilter STASH_FILTER = new SequenceChatFilter(
+            "materials stashed away",
+            "types of materials stashed",
+            "to pick them up"
+    );
 
     @Override
     public boolean isEnabled() {
         return ChatConfig.STASH_MESSAGES_REMOVER_TOGGLE;
     }
 
-//    We need to consider whether we store this to a DataStorage interface or just keep it to a per-boot level
+    // We need to consider whether we store this to a DataStorage interface or just keep it to a per-boot level
     public boolean stashPreviouslyClaimed = false;
-
-    private transient ArrayList<String> pastMessages = new ArrayList<>();
 
     public StashMessagesRemover() {}
 
-    private void registerStashClaimDetector() {
-        ClientReceiveMessageEvents.GAME.register((message, overlay) -> {
-            if (message.getString().contains("You picked up")
-                    && message.getString().contains("from your material stash")
-                    && !stashPreviouslyClaimed) {
-                stashPreviouslyClaimed = true;
-                Util.tickExecuteLater(2, () -> PlayerActionUtil.notifyAll(
-                        "TIP - To claim stash more easily and quickly, use the Stash Helper keybind, " +
-                                "which closes the bazaar and claims your stash! To disable stash messages, " +
-                                "enable the \"Disable Stash Messages\" option in the Bazaar Utils config."));
-            }
-        });
-    }
+    @Subscription
+    @OnlyOnSkyBlock
+    public void onChat(ChatReceivedEvent.Pre event) {
+        String message = event.getText();
 
-    private void registerStashRemover() {
-        ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
-            if (!isEnabled() || message.getString().contains("Mana")) {
-                return true;
-            }
+        if (message.contains("You picked up") && message.contains("from your material stash") && !stashPreviouslyClaimed) {
+            stashPreviouslyClaimed = true;
 
-            String currentMessageString = message.getString();
-            int currentMessageRoleIndex = indexOfMessage(currentMessageString);
-            boolean removeCurrentMessage;
-
-            if (currentMessageRoleIndex == -1) {
-                pastMessages.clear();
-                removeCurrentMessage = false;
-            } else if (currentMessageRoleIndex == pastMessages.size()) {
-                pastMessages.add(currentMessageString);
-                removeCurrentMessage = true;
-
-                if (pastMessages.size() == SEQUENCE_LENGTH) {
-                    pastMessages.clear();
-                }
-            } else {
-                pastMessages.clear();
-                removeCurrentMessage = false;
-
-                if (currentMessageRoleIndex == 0) {
-                    pastMessages.add(currentMessageString);
-                    removeCurrentMessage = true;
-                }
-            }
-
-            return !removeCurrentMessage;
-        });
-    }
-
-    private static int indexOfMessage(String messageContent) {
-        for (Map.Entry<String, Integer> entry : REMOVE_MAP.entrySet()) {
-            if (messageContent.contains(entry.getKey())) {
-                return entry.getValue();
-            }
+            Util.tickExecuteLater(2, () -> PlayerLogger.send(
+                    "TIP - To claim stash more easily, use the Stash Helper keybind. " +
+                            "To disable stash messages, enable \"Disable Stash Messages\" in the Bazaar Utils config."));
+            return;
         }
 
-        return -1;
-    }
+        if (!isEnabled() || message.contains("Mana")) return;
 
-    @Override
-    protected void registerFabricEvents() {
-        registerStashRemover();
-        registerStashClaimDetector();
+        if (STASH_FILTER.shouldRemove(message)) {
+            event.cancel();
+        }
     }
 }
--- src/main/java/com/github/mkram17/bazaarutils/features/chat/UselessBazaarNotificationsRemover.java
+++ src/main/java/com/github/mkram17/bazaarutils/features/chat/UselessBazaarNotificationsRemover.java
@@ -2,20 +2,24 @@ package com.github.mkram17.bazaarutils.features.chat;
 
 import com.github.mkram17.bazaarutils.config.features.chat.ChatConfig;
 import com.github.mkram17.bazaarutils.events.BUListener;
-import com.github.mkram17.bazaarutils.utils.PlayerActionUtil;
+import com.github.mkram17.bazaarutils.utils.PlayerLogger;
 import com.github.mkram17.bazaarutils.utils.Util;
+import com.github.mkram17.bazaarutils.utils.annotations.events.OnlyWhenEnabled;
 import com.github.mkram17.bazaarutils.utils.annotations.modules.Module;
 import com.github.mkram17.bazaarutils.utils.config.ToggleableFeature;
 import com.teamresourceful.resourcefulconfig.api.types.info.TooltipProvider;
+import com.teamresourceful.resourcefulconfig.api.types.info.Translatable;
 import lombok.Getter;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.network.chat.Component;
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription;
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock;
+import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent;
 
 import java.util.Arrays;
 
 @Module
 public class UselessBazaarNotificationsRemover extends BUListener implements ToggleableFeature {
-    public enum TransientBazaarMessages implements TooltipProvider {
+    public enum TransientBazaarMessages implements Translatable, TooltipProvider {
         CANCELLING_ORDER("[Bazaar] Cancelling order..."),
         PUTTING_GOODS_IN_ESCROW("[Bazaar] Putting goods in escrow..."),
         SUBMITTING_BUY_ORDER("[Bazaar] Submitting buy order..."),
@@ -33,8 +37,13 @@ public class UselessBazaarNotificationsRemover extends BUListener implements Tog
         }
 
         @Override
+        public String getTranslationKey() {
+            return "bazaarutils.hypixel.messages.transient_bazaar_notifications." + name().toLowerCase() + ".label";
+        }
+
+        @Override
         public Component getTooltip() {
-            return Component.nullToEmpty(getMessage());
+            return Component.nullToEmpty(message);
         }
     }
 
@@ -47,38 +56,30 @@ public class UselessBazaarNotificationsRemover extends BUListener implements Tog
         return ChatConfig.USELESS_BAZAAR_NOTIFICATIONS_REMOVER_EXCLUDED_NOTIFICATIONS;
     }
 
-    //    We need to consider whether we store this to a DataStorage interface or just keep it to a per-boot level
+    // We need to consider whether we store this to a DataStorage interface or just keep it to a per-boot level
     public boolean firstTimeRemoved = true;
 
     public UselessBazaarNotificationsRemover() {}
 
-    private void registerUselessNotificationDetector(){
-        ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
-            if (!isEnabled()) {
-                return true;
-            }
+    @Subscription
+    @OnlyOnSkyBlock
+    @OnlyWhenEnabled
+    public void onChat(ChatReceivedEvent.Pre event) {
+        String message = event.getText();
+        if (!isNotificationUseless(message)) return;
 
-            if (isNotificationUseless(message.getString())) {
-                if (firstTimeRemoved) {
-                    firstTimeRemoved = false;
+        if (firstTimeRemoved) {
+            firstTimeRemoved = false;
 
-                    Util.tickExecuteLater(2, () -> PlayerActionUtil.notifyAll("TIP - Useless Bazaar notifications such as \"Putting goods in escrow...\" are removed by default! " +
-                            "To disable this feature, uncheck the \"Remove Useless Bazaar Notifications\" option in the Bazaar Utils settings."));
-                }
+            Util.tickExecuteLater(2, () -> PlayerLogger.send(
+                    "TIP - Useless Bazaar notifications are removed by default! " +
+                            "To disable, uncheck \"Remove Useless Bazaar Notifications\" in Bazaar Utils settings."));
+        }
 
-                return false;
-            }
-
-            return true;
-        });
+        event.cancel();
     }
 
     private boolean isNotificationUseless(String message) {
         return Arrays.stream(getExcludedNotifications()).anyMatch(n -> message.contains(n.getMessage()));
-    }
-
-    @Override
-    protected void registerFabricEvents() {
-        registerUselessNotificationDetector();
     }
 }
