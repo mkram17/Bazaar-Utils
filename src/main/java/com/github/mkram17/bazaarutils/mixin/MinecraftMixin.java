74973dbd346baff68591a4de297303169142125c
0xar-ds <sadden-rope-sleek@duck.com>
2026-05-24 11:42:28 -0300

    ScreenChangeEvent -> ScreenChangeEvent.Pre & Post
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/mixin/MinecraftMixin.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/MinecraftMixin.java
@@ -2,6 +2,8 @@ package com.github.mkram17.bazaarutils.mixin;
 
 import com.github.mkram17.bazaarutils.BazaarUtils;
 import com.github.mkram17.bazaarutils.events.minecraft.ScreenChangeEvent;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import org.jetbrains.annotations.Nullable;
@@ -19,7 +21,13 @@ public class MinecraftMixin {
     public Screen screen;
 
     @Inject(method = "setScreen", at = @At("HEAD"))
-    public void setScreen(Screen newScreen, CallbackInfo ci) {
-        new ScreenChangeEvent(screen, newScreen).post(BazaarUtils.EVENT_BUS);
+    private void setScreenPre(Screen newScreen, CallbackInfo ci, @Share("oldScreen") LocalRef<Screen> oldScreenRef) {
+        oldScreenRef.set(this.screen);
+        new ScreenChangeEvent.Pre(this.screen, newScreen).post(BazaarUtils.EVENT_BUS);
+    }
+
+    @Inject(method = "setScreen", at = @At("RETURN"))
+    private void setScreenPost(Screen newScreen, CallbackInfo ci, @Share("oldScreen") LocalRef<Screen> oldScreenRef) {
+        new ScreenChangeEvent.Post(oldScreenRef.get(), newScreen).post(BazaarUtils.EVENT_BUS);
     }
 }
