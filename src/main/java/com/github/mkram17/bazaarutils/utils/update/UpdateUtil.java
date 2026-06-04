d415a49e6a2984222c66c1b95de57ee2102e6133
0xar-ds <sadden-rope-sleek@duck.com>
2026-06-04 13:12:35 -0300

    Drop AdvancedSettings
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- src/main/java/com/github/mkram17/bazaarutils/utils/update/UpdateUtil.java
+++ src/main/java/com/github/mkram17/bazaarutils/utils/update/UpdateUtil.java
@@ -1,7 +1,7 @@
 package com.github.mkram17.bazaarutils.utils.update;
 
 import com.github.mkram17.bazaarutils.BazaarUtils;
-import com.github.mkram17.bazaarutils.config.features.AdvancedConfig;
+import com.github.mkram17.bazaarutils.config.BUConfig;
 import com.github.mkram17.bazaarutils.config.hidden.MetadataConfig;
 import com.github.mkram17.bazaarutils.config.util.ConfigUtil;
 import com.github.mkram17.bazaarutils.utils.PlayerActionUtil;
@@ -97,7 +97,7 @@ public final class UpdateUtil {
                             return CompletableFuture.completedFuture(null);
                         }
 
-                        if (AdvancedConfig.AUTO_UPDATE_TOGGLE) {
+                        if (BUConfig.AUTOMATIC_UPDATES_TOGGLE) {
                             return update.launchUpdate().thenRun(() -> PlayerActionUtil.notifyAll("Update downloaded! Restart to apply."));
                         } else {
                             PlayerActionUtil.notifyAll(
