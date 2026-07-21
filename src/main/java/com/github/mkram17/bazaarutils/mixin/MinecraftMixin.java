c96c44e73a27782b6de71b9236f420b876130ca3
mkram17 <1maxkramer@gmail.com>
2026-07-21 15:50:52 -0400

    fix mixin bug

--- src/main/java/com/github/mkram17/bazaarutils/mixin/MinecraftMixin.java
+++ src/main/java/com/github/mkram17/bazaarutils/mixin/MinecraftMixin.java
@@ -2,7 +2,11 @@ package com.github.mkram17.bazaarutils.mixin;
 
 import com.github.mkram17.bazaarutils.BazaarUtils;
 import com.github.mkram17.bazaarutils.events.ScreenChangeEvent;
-import net.minecraft.client.Minecraft;
+//? if >=26.2 {
+import net.minecraft.client.gui.Gui;
+//?} else {
+/*import net.minecraft.client.Minecraft;
+*///?}
 import net.minecraft.client.gui.screens.Screen;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +15,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//for ScreenChangeEvent
-@Mixin(value = Minecraft.class)
+//for ScreenChangeEvent; 26.2 moved the screen field and setScreen from Minecraft to Gui
+//? if >=26.2 {
+@Mixin(Gui.class)
+//?} else {
+/*@Mixin(Minecraft.class)
+*///?}
 public class MinecraftMixin {
     @Shadow
     @Nullable
-    public Screen screen;
+    //? if >=26.2 {
+    private Screen screen;
+    //?} else {
+    /*public Screen screen;
+    *///?}
 
     @Inject(method = "setScreen", at = @At("HEAD"))
     public void setScreen(Screen newScreen, CallbackInfo ci) {
