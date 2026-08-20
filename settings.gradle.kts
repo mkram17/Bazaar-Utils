68c3206712e94da260190123ae29caaf6961f7ef
0xar-ds <sadden-rope-sleek@duck.com>
2026-08-20 23:50:44 +0200

    Drop 1.21.11 & add 26.1.2
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ plugins {
 stonecutter {
     kotlinController = true
     centralScript = "build.gradle.kts"
-    create(rootProject){
-        versions("1.21.11")
-        vcsVersion = "1.21.11"
+    create(rootProject) {
+        versions("26.1.2")
+        vcsVersion = "26.1.2"
     }
 }
 
